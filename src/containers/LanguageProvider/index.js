64b932aebcfcce060528cba19993aede36eb0a6c
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-05-04 20:32:45 -0700

    add intl polyfill for safari browser

--- src/containers/LanguageProvider/index.js
+++ src/containers/LanguageProvider/index.js
@@ -4,11 +4,11 @@
  *
  */
 
+import { IntlProvider } from 'react-intl';
 import React from 'react';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
-import { IntlProvider } from 'react-intl';
 
 import actions from '../../actions';
 
--- src/index.js
+++ src/index.js
@@ -8,9 +8,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './registerServiceWorker';
+import { translationMessages } from './i18n';
 
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+// Intl polyfill for browsers without Intl support
+if (!window.Intl) {
+  new Promise(resolve => {
+    resolve(import('intl'));
+  })
+    .then(() =>
+      Promise.all([
+        import('intl/locale-data/jsonp/en.js'),
+        import('intl/locale-data/jsonp/ar.js'),
+      ]),
+    ) /* eslint-disable */
+    .then(() => render(translationMessages))
+    .catch(err => {
+      throw err;
+    });
+} else {
+  render(translationMessages);
+}
 
 registerServiceWorker();
